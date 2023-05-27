d7aab867be48fde05f23725df887d06cd7a34ff7
Eric Nelson <ernelson12o0@gmail.com>
2023-05-27 16:27:33 -0400

    Lab Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+function App() {
+    const [imgSrc, setImSrc] =useState('')
+
+    useEffect(()=> {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setImSrc(data.message)
+        })
+    }, [])
+
+    return (imgSrc === '' ? <p>Loading...</p> : <img src={imgSrc} alt="A Random Dog" />);
+    
+}
+
+export default App;
+
+
+
